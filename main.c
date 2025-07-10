9fea31a3755e1e3e1b18ab40037feffb97c5d2ae
SoraiaEvelin <soraiaevelinsantiagosilva@gmail.com>
2025-07-10 12:01:28 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+item A
+int main()
+{
+    printf("**********\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("**********\n");
+
+    printf("***\n");
+printf(  "*     *\n");
+printf(" *       *\n");
+printf("*        *\n");
+printf("*        *\n");
+printf(" *       *\n");
+    printf("***\n");
+
+    printf("*\n");
+   printf("***\n");
+printf("  ****\n");
+printf(" *****\n");
+printf("*******\n");
+printf("*********\n");
+printf("    * \n");
+printf("    * \n");
+
+
+ printf("   *  \n");
+ printf(" *   *\n");
+ printf("*     *\n");
+printf("*       *\n");
+printf(" *     *\n");
+printf("    * \n");
+
+
+
+
+    return 0;
+}
